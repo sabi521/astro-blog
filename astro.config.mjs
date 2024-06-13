2f292a583add2e5637503f57bbe88513c256450b
Sarbjeet Singh <sabi521@gmail.com>
2024-06-12 20:28:49 -0400

    new changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel()
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
